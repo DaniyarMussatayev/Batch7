d8219fcf5f224aba1d26ee1d9cce2d689290b3ef
DaniyarMussatayev <mussatayevdt@gmail.com>
2020-07-08 18:42:23 +0600

    Local, Instance, Static methods

--- src/com/syntax/class18/Employee.java
+++ src/com/syntax/class18/Employee.java
@@ -6,26 +6,25 @@ package com.syntax.class18;
 //Print out the eID , salary and  CEO for each of the objects
 
 public class Employee {
+
+	int empId;
+	double salary;
+	
+	static String CEO="Sumair";
+	
 	public static void main(String[] args) {
-		Employee obj1=new Employee();
-		obj1.eID = 17;
-		obj1.salary = 80000;
-		System.out.println("eID is " + obj1.eID);
-		System.out.println("salary is "+obj1.salary);
-		System.out.println("CEO is" +Employee.CEO);
 		
-		Employee obj2 =new Employee();
-		obj2.eID = 6;
-		obj2.salary = 90000;
-		System.out.println("eID is " + obj2.eID);
-		System.out.println("salary is "+obj2.salary);
-		System.out.println("CEO is" +Employee.CEO);
+		System.out.println(Employee.CEO);
+		
+		//accessing static variables within same class
+		System.out.println(CEO);
+		
+		//accessing static method
+		sayHelloToCeo();
 	}
 	
-	
-	
-	int eID;
-	int salary;
-	static String CEO = "Sumair";
+	static void sayHelloToCeo() {
+		System.out.println("Hi "+CEO);
+	}
 
 }
