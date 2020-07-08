53754f1ec56e69451d7159db83d810c6da5dc7df
DaniyarMussatayev <mussatayevdt@gmail.com>
2020-07-08 18:35:59 +0600

    Different Methods, Methods Return Value

--- src/com/syntax/class16/CalculatorTest.java
+++ src/com/syntax/class16/CalculatorTest.java
@@ -1,17 +1,25 @@
 package com.syntax.class16;
 
 public class CalculatorTest {
-	public static void main(String[] args) {
-		
-		// call method add();
-		
-		Calculator calc = new Calculator();
-		calc.add(100, 200);
-		calc.sub(10, 20);
-		calc.div(100, 3);
-		calc.mult(12, 45);
-		
-		
-	}
+		public static void main(String[] args) {
+			
+			//call method add();
+			Calculator c=new Calculator();
+			c.add(100, 200);
+			c.add(10, 20);
+			c.add(1000, 2000);
+			
+			c.sub(100, 10);
+			
+			c.div(10, 2);
+			
+			c.mult(10, 10);
+			
+			//c.mult(10); CE: number of values must match number of parameters
+			
+			//c.mult(10.99, 9.99); CE: values must match type of parameters
+
+			//BREAK TILL 8:40
+		}
 
 }
--- src/com/syntax/class16/MethodsWithReturnValue.java
+++ src/com/syntax/class16/MethodsWithReturnValue.java
@@ -2,31 +2,37 @@ package com.syntax.class16;
 
 public class MethodsWithReturnValue {
 	public static void main(String[] args) {
+
+		// create a String and based and on the number of character define whether
+		// String is long or short
+		// long if length >10, short if less
+
 		String str = "I love Java";
 
 		int length = str.length();
-
+		
 		if (length >= 10) {
-			System.out.println("created String is long");
+			System.out.println("Created String is long");
 		} else {
 			System.out.println("Created String is short");
 		}
-
-		// define which number is the largest and define whether this largest number is
-		// even or odd
-
-		DifferentMethods obj = new DifferentMethods();
-		obj.math(10, 20);
-
+		
+		//identify which number is the largest and then define whether this largest number if even or odd
+		TaskMethods obj=new TaskMethods();
+		obj.larger(10, 20);
+		
 	}
-
-	int largest(int num1, int num2) {
+	
+	//create a method that will RETURN the largest number
+	int largest(int a, int b) {
 		int largest;
-		if (num1 > num2) {
-			largest = num1;
-		} else {
-			largest = num2;
+		
+		if(a>b) {
+			largest=a;
+		}else {
+			largest=b;
 		}
+		
 		return largest;
 	}
 
