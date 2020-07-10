63c64fae78015f7fda1ca9730600643d328fa886
DaniyarMussatayev <mussatayevdt@gmail.com>
2020-07-10 13:33:45 +0600

    Example code

---
+++ src/com/syntax/class19_1/AccessingEmployeeMembers.java
@@ -0,0 +1,20 @@
+package com.syntax.class19_1;
+
+import com.syntax.class19.Employee;
+//to import shortcut is cmd+shif+o for Mac
+//ctrl+shif+o for Windows
+public class AccessingEmployeeMembers {
+	public static void main(String[] args) {
+
+		Employee emp = new Employee();
+		emp.lastName = "Oliva";// accessing public variables within different package
+		emp.name = "Luis";// accessing public variables within different package
+
+//		emp.age --> protected not visible within different package
+//		emp.salary--> default not visible within different package
+//		emp.ssn--> private not visible within different package
+
+		emp.displayName();// public method
+
+	}
+}
